029118bb8b6436b7fd09d0b4c90084dbd9313a6f
thebroccolibob <thebroccolibob@gmail.com>
2024-04-19 19:43:07 -0700

    Initial commit

---
+++ src/main/kotlin/io/github/thebroccolibob/soulorigins/Soulorigins.kt
@@ -0,0 +1,15 @@
+package io.github.thebroccolibob.soulorigins
+
+import net.fabricmc.api.ModInitializer
+import org.slf4j.LoggerFactory
+
+object Soulorigins : ModInitializer {
+    private val logger = LoggerFactory.getLogger("soul-origins")
+
+	override fun onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		logger.info("Hello Fabric world!")
+	}
+}
---
+++ src/main/kotlin/io/github/thebroccolibob/soulorigins/SouloriginsDataGenerator.kt
@@ -0,0 +1,9 @@
+package io.github.thebroccolibob.soulorigins
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object SouloriginsDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
