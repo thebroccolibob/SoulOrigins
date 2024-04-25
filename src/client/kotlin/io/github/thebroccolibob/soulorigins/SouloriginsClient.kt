39c560e1a2358ec51d25399d11f2585bb4de0435
thebroccolibob <thebroccolibob@gmail.com>
2024-04-24 22:44:28 -0700

    Realized owo-lib doesn't provide the UI I need

--- src/client/kotlin/io/github/thebroccolibob/soulorigins/SouloriginsClient.kt
+++ src/client/kotlin/io/github/thebroccolibob/soulorigins/SouloriginsClient.kt
@@ -1,21 +1,9 @@
 package io.github.thebroccolibob.soulorigins
 
-import io.github.thebroccolibob.soulorigins.screen.LaBaraja
 import net.fabricmc.api.ClientModInitializer
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
-import net.minecraft.client.option.KeyBinding
-import org.lwjgl.glfw.GLFW
 
 object SouloriginsClient : ClientModInitializer {
-	private val TEST = KeyBinding("advancements.adventure.root.title", GLFW.GLFW_KEY_H, "gamerule.category.misc")
-
 	override fun onInitializeClient() {
-		KeyBindingHelper.registerKeyBinding(TEST)
-		ClientTickEvents.END_CLIENT_TICK.register {client ->
-			while(TEST.wasPressed()) {
-				client.setScreen(LaBaraja())
-			}
-		}
+
 	}
 }
--- src/client/kotlin/io/github/thebroccolibob/soulorigins/screen/LaBaraja.kt
+++
@@ -1,9 +0,0 @@
-package io.github.thebroccolibob.soulorigins.screen
-
-import io.wispforest.owo.ui.base.BaseUIModelScreen
-import io.wispforest.owo.ui.container.FlowLayout
-import net.minecraft.util.Identifier
-
-class LaBaraja : BaseUIModelScreen<FlowLayout>(FlowLayout::class.java, DataSource.asset(Identifier("soul-origins", "la_baraja"))){
-    protected override fun build(rootComponent: FlowLayout?) {}
-}
