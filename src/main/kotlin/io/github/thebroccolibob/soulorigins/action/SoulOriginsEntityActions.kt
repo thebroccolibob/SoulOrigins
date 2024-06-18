4c8e99e88f266465a689d41273f6e33baec051c2
JosiahFu <josiahfu@gmail.com>
2024-06-17 21:01:57 -0700

    Add neutron & extra keybinds as compile dependencies, add neutron code to neutral power

--- src/main/kotlin/io/github/thebroccolibob/soulorigins/action/SoulOriginsEntityActions.kt
+++ src/main/kotlin/io/github/thebroccolibob/soulorigins/action/SoulOriginsEntityActions.kt
@@ -1,11 +1,13 @@
 package io.github.thebroccolibob.soulorigins.action
 
+import archives.tater.neutron.NeutronState
 import io.github.apace100.apoli.data.ApoliDataTypes
 import io.github.apace100.apoli.power.CooldownPower
 import io.github.apace100.apoli.power.PowerType
 import io.github.apace100.apoli.power.factory.action.ActionFactory
 import io.github.apace100.apoli.registry.ApoliRegistries
 import io.github.apace100.calio.data.SerializableData
+import io.github.apace100.calio.data.SerializableDataType
 import io.github.apace100.calio.data.SerializableDataTypes
 import io.github.thebroccolibob.soulorigins.SerializableData
 import io.github.thebroccolibob.soulorigins.SoulOrigins
@@ -15,7 +17,9 @@ import io.github.thebroccolibob.soulorigins.syncPower
 import net.minecraft.entity.AreaEffectCloudEntity
 import net.minecraft.entity.Entity
 import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.MovementType
 import net.minecraft.entity.effect.StatusEffectInstance
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.ItemStack
 import net.minecraft.item.PotionItem
 import net.minecraft.nbt.NbtElement
@@ -136,4 +140,29 @@ fun registerSoulOriginsEntityActions() {
     }
 
     register(SpawnEntityRaycastAction.factory)
+
+    register("use_riptide", SerializableData {
+        add("duration", SerializableDataTypes.INT, 20)
+    }) { data, entity ->
+        (entity as? PlayerEntity)?.useRiptide(data.getInt("duration"))
+    }
+
+    register("move", SerializableData {
+        add("move", SerializableDataTypes.VECTOR)
+        add("movement_type", SerializableDataType.enumValue(MovementType::class.java), MovementType.SELF)
+    }) { data, entity ->
+        entity.move(data.get("movement_type"), data.get("move"))
+    }
+
+    register("set_neutron", SerializableData {
+        add("enabled", SerializableDataTypes.BOOLEAN, true)
+    }) { data, entity ->
+        entity.server?.let(NeutronState.Companion::get)?.run {
+            if (data.getBoolean("enabled")) {
+                enabledPlayers.add(entity.uuid)
+            } else {
+                enabledPlayers.remove(entity.uuid)
+            }
+        }
+    }
 }
