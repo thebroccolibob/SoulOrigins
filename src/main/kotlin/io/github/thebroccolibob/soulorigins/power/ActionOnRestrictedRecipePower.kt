f788ae7d7fb845e4f9ceb94b557a837c4b60d05e
JosiahFu <josiahfu@gmail.com>
2024-06-26 23:50:08 -0700

    wip recipe action power

---
+++ src/main/kotlin/io/github/thebroccolibob/soulorigins/power/ActionOnRestrictedRecipePower.kt
@@ -0,0 +1,8 @@
+package io.github.thebroccolibob.soulorigins.power
+
+import io.github.apace100.apoli.power.Power
+import io.github.apace100.apoli.power.PowerType
+import net.minecraft.entity.LivingEntity
+
+class ActionOnRestrictedRecipePower(type: PowerType<*>, entity: LivingEntity?) : Power(type, entity) {
+}
